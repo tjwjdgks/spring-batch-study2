81942f98a56d5689c99b30a9f89f670fcfacf9d0
enzo-han <enzo.han@kakaocorp.com>
2022-08-01 11:23:54 +0900

    init

---
+++ src/main/java/com/example/springbatchbase/SpringBatchBaseApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbatchbase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBatchBaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBatchBaseApplication.class, args);
+    }
+
+}
